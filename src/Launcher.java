0959eb77104fd6d1125164c85cebcc8ad0474e6f
Francois Nguinbu <fry91800@gmail.com>
2022-02-11 16:54:16 +0100

    fibo added

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,8 +5,27 @@ class Launcher {
        java.util.Scanner scan = new java.util.Scanner(System.in);
        String command = scan.nextLine();
        while (!(command.equals("quit"))) {
-           System.out.println("unknown Command");
+           if (command.equals("fibo")) {
+               System.out.println("Entrez le rang de la suite de fibonacci pour obtenir son image");
+               int nb = Integer.parseInt(scan.nextLine());
+               int res = fibo(nb);
+               System.out.println(res);
+
+           }
+           else {
+               System.out.println("unknown Command");
+           }
            command = scan.nextLine();
        }
+   }
+   private static int fibo(int n){
+       if (n<0) {
+           throw new IllegalArgumentException("Should be positive");
+       }
+
+       if(n == 0 || n == 1) {
+           return n;
+       }
+       return fibo(n-1) + fibo(n - 2);
    }
 }
