832f3f563609fb083a91f0ce8a9ab74c0693069e
Francois Nguinbu <fry91800@gmail.com>
2022-02-11 18:38:24 +0100

    freq bancale

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class Launcher {
-   public static void main (String[] args){
+   public static void main (String[] args) throws IOException {
        System.out.println("Binvenue dans le compilateur");
 
        java.util.Scanner scan = new java.util.Scanner(System.in);
@@ -11,6 +19,15 @@ class Launcher {
                int res = fibo(nb);
                System.out.println(res);
 
+           }
+           if (command.equals("freq")){
+               System.out.println("Entrez un chemin de fichier, renvois les 3 mots les plus frequents de celui ci");
+               String strPath = scan.nextLine();
+                   Path path = Paths.get(strPath);
+               String text = java.nio.file.Files.readString(path);
+               text.toLowerCase();
+               List<String> mostFrequent = threeMostFrequentWords(text);
+               mostFrequent.forEach(System.out::println);
            }
            else {
                System.out.println("unknown Command");
@@ -27,5 +44,52 @@ class Launcher {
            return n;
        }
        return fibo(n-1) + fibo(n - 2);
+   }
+   private static List<String> threeMostFrequentWords(String text){
+       //creer un hashmap
+       Map<String, Integer> wordsCounter = new HashMap<String, Integer>();
+       //split le text en tableau de mots
+       String[] words = splitTextToWords(text);
+       //pour chaque mots ajouter le couple mot -> nb d'occurence
+       for (String word: words) {
+           if (keyExistsInHashMap(word, wordsCounter)){
+               wordsCounter.put(word,countWords(word, words));
+           }
+       }
+       //Tant que most frequent n'est pas de taille 3 il se nourrit du plus grand elt de la hashmap
+       List<String> mostFrequent = new ArrayList<String>();
+       while ((mostFrequent.size() < 3) || wordsCounter.size() < 0){
+           String currentMostFrequent = mapMostFrequent(wordsCounter);
+           mostFrequent.add(currentMostFrequent);
+           wordsCounter.remove(currentMostFrequent);
+       }
+       return mostFrequent;
+
+   }
+   private static String[] splitTextToWords(String text) {
+       return text.split(" ");
+   }
+   private static int countWords(String str, String[] wordList){
+       int res = 0;
+       for (String word: wordList) {
+           if (str.equals(word)){
+               res += 1;
+           }
+       }
+       return res;
+   }
+   private static boolean keyExistsInHashMap(String key, Map<String, Integer> hashMap) {
+    return true;
+   }
+   private static String mapMostFrequent(Map<String, Integer> hashMap){
+       int highest  = 0;
+       String res = "null";
+       for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+           if (highest < entry.getValue()){
+               highest = entry.getValue();
+               res = entry.getKey();
+           }
+       }
+       return res;
    }
 }
